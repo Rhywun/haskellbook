dfc65322ffce577d03b4cf50120b6977955e6f34
Patrick Huffer <rhywun@rhywun.com>
2018-04-19 07:40:48 -0400

    Middle of Chapter13

--- Chapter13/Scratch.hs
+++ Chapter13/Scratch.hs
@@ -10,3 +10,19 @@ module Scratch where
  b. Filesystem
  c. Control.Monad
 -}
+--
+-- `return` puts a value into a Monad so it can be returned from IO
+--
+twoo :: IO Bool
+twoo = do
+  c <- getChar
+  c' <- getChar
+  return (c == c')
+
+main :: IO ()
+main = do
+  c <- getChar
+  c' <- getChar
+  if c == c'
+    then putStrLn "True"
+    else return ()
