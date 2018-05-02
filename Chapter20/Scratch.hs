d89c7a97ca331d6ac71755715b5d1098ef4c6df2
Patrick Huffer <rhywun@rhywun.com>
2018-05-01 21:53:56 -0400

    Middle of Chapter20

--- Chapter20/Scratch.hs
+++ Chapter20/Scratch.hs
@@ -3,6 +3,9 @@ module Scratch where
 import           Data.Foldable
 import           Data.Monoid
 
+--
+-- 20.4 - Demonstrating Foldable instances
+--
 -- Identity
 --
 newtype Identity a =
@@ -22,11 +25,20 @@ data Optional a
   deriving (Eq, Show)
 
 instance Foldable Optional where
-  foldr _ z Nada = z
+  foldr _ z Nada    = z
   foldr f z (Yep x) = f x z
-
-  foldl _ z Nada = z
+  foldl _ z Nada    = z
   foldl f z (Yep x) = f z x
-
-  foldMap _ Nada = mempty
+  foldMap _ Nada    = mempty
   foldMap f (Yep a) = f a
+--
+--
+-- 20.5 - Some basic derived operations
+--
+-- Exercises: Library Functions
+--
+-- 1
+sum' :: (Num a, Foldable t) => t a -> a
+sum' = undefined
+
+-- Yeah, I dunno.
