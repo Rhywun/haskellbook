6e8b26f37bc4e57e117fc6359709103b4e349637
Patrick Huffer <rhywun@rhywun.com>
2018-09-18 20:30:45 -0400

    Revisited Chapter10

--- Chapter10/Exercises.hs
+++ Chapter10/Exercises.hs
@@ -3,54 +3,150 @@ module Chapter10.Exercises where
 --
 -- Warm-up and review
 --
+
 -- 1
---
+
 stops = "pbtdkg"
 
 vowels = "aeiou"
 
 -- a
 threeTuples :: [(Char, Char, Char)]
-threeTuples = [(x, y, z) | x <- stops, y <- vowels, z <- stops]
+threeTuples = [ (x, y, z) | x <- stops, y <- vowels, z <- stops ]
 
-threeStrings = [[x, y, z] | x <- stops, y <- vowels, z <- stops]
+threeStrings :: [String]
+threeStrings = [ [x, y, z] | x <- stops, y <- vowels, z <- stops ]
 
 -- b
 threeTuples' :: [(Char, Char, Char)]
-threeTuples' = [('p', y, z) | y <- vowels, z <- stops]
+threeTuples' = [ ('p', y, z) | y <- vowels, z <- stops ]
 
-threeStrings' = [['p', y, z] | y <- vowels, z <- stops]
+threeStrings' :: [String]
+threeStrings' = [ ['p', y, z] | y <- vowels, z <- stops ]
 
 -- c
---
+
 nouns = ["cat", "dog", "human"]
 
 verbs = ["eats", "loves", "pets"]
 
 threeTuples'' :: [(String, String, String)]
-threeTuples'' = [(x, y, z) | x <- nouns, y <- verbs, z <- nouns]
+threeTuples'' = [ (x, y, z) | x <- nouns, y <- verbs, z <- nouns ]
+
+threeStrings'' :: [String]
+threeStrings'' = [ x ++ " " ++ y ++ " " ++ z | x <- nouns, y <- verbs, z <- nouns ]
 
 -- 2
+
 {-
-seekritFunc "now is the time" == 3
-seekritFunc "resistance is futile" == 6
+seekritFunc "now is the time"      -- 3
+seekritFunc "resistance is futile" -- 6
+seekritFunc "now is the time for all good me to come to the aid of their party" -- 3
 -}
 seekritFunc :: String -> Int
 seekritFunc x = div (sum (map length (words x))) (length (words x))
   -- Give the average length of a word in a string
 
+-- 3
+
+{-
+seekritFunc' "now is the time"      -- 3.0
+seekritFunc' "resistance is futile" -- 6.0
+seekritFunc' "now is the time for all good me to come to the aid of their party" -- 3.125
+-}
 seekritFunc' :: String -> Double
 seekritFunc' x =
-  (fromIntegral (sum (map length (words x)))) /
-  (fromIntegral (length (words x)))
+  fromIntegral (sum (map length (words x))) / fromIntegral (length (words x))
 
 --
 -- Rewriting functions using folds
 --
+
+{-
+and' [True, False, True] -- False
+-}
+and' :: [Bool] -> Bool
+and' = foldr (&&) True
+
+{-
+or' [True, False, True] -- True
+-}
 or' :: [Bool] -> Bool
 or' = foldr (||) False
 
+{-
+any' even [1,3,5] -- False
+any' odd [1,3,5]  -- True
+-}
 any' :: (a -> Bool) -> [a] -> Bool
-any' = undefined
+-- any' _ [] = False
+-- any' p (x:xs) = if p x then True else any'' p xs
+-- any'' p (x:xs) = p x || any'' p xs
+any' p = foldr ((||) . p) False -- Cheated - how does this follow?
 
--- Totally lost here.
+{-
+elem' 1 [1..10] -- True
+elem' 1 [2..10] -- False
+-}
+elem' :: Eq a => a -> [a] -> Bool
+-- elem' _ [] = False
+-- elem' n (x : xs) = if n == x then True else elem' n xs
+-- elem' n (x : xs) = n == x || elem' n xs
+elem' x = foldr ((||) . (== x)) False
+
+{-
+elem'' 1 [1..10] -- True
+elem'' 1 [2..10] -- False
+-}
+elem'' :: Eq a => a -> [a] -> Bool
+elem'' x = any' (== x)
+
+{-
+reverse' "blah" -- "halb"
+reverse' [1..5] -- [5,4,3,2,1]
+-}
+reverse' :: [a] -> [a]
+-- reverse' [x     ] = [x]
+-- reverse' (x : xs) = reverse' xs ++ [x]
+reverse' = foldl (flip (:)) []
+
+{-
+map' (+1) [1,2,3] -- [2,3,4]
+-}
+map' :: (a -> b) -> [a] -> [b]
+-- map' _ []       = []
+-- map' f (x : xs) = f x : map' f xs
+map' f = foldr ((:) . f) [] -- Cheated
+
+{-
+filter' (> 2) [1,2,3,4] -- [3,4]
+-}
+filter' :: (a -> Bool) -> [a] -> [a]
+-- filter' _ []       = []
+-- filter' p (x : xs) = if p x then x : filter' p xs else filter' p xs
+filter' p = foldr (\a b -> if p a then a : b else b) [] -- Cheated but I get it
+
+{-
+squish [[1, 2], [3, 4]] -- [1, 2, 3, 4]
+-}
+squish :: [[a]] -> [a]
+-- squish [x] = [x] -- <-- Error, no idea how to write this
+-- squish (x:xs) = x ++ squish xs
+squish = foldr (++) [] -- Cheated
+
+{-
+squishMap (\x -> [1, x, 3]) [2]                 -- [1,2,3]
+squishMap (\x -> "WO " ++ [x] ++ " OT ") "blah" -- "WO b OT WO l OT WO a OT WO h OT "
+-}
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f = foldr ((++) . f) []
+
+{-
+squishAgain [[1, 2], [3, 4]] -- [1, 2, 3, 4]
+-}
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+maximumBy :: (a -> a -> Ordering) -> [a] -> a
+maximumBy = undefined
+-- PASS
--- Chapter10/Scratch.hs
+++ Chapter10/Scratch.hs
@@ -3,8 +3,9 @@ module Chapter10.Scratch where
 import           Data.Time
 
 --
--- Fold right
+-- 10.4 - Fold right
 --
+
 e01 = map (+ 1) [1, 2, 3]
 
 e02 = foldr (+) 0 [1, 2, 3]
@@ -12,26 +13,29 @@ e02 = foldr (+) 0 [1, 2, 3]
 
 -- This function returns a True result as soon as it finds one True value,
 -- so it does not require the evaluation of every value in the list...
+-- ...therefore it can be applied even to an infinite list
+{-
+myAny even [1 ..] -- True
+-}
 myAny :: (a -> Bool) -> [a] -> Bool
 myAny f = foldr (\x b -> f x || b) False
 
--- ...therefore it can be applied even to an infinite list
-{-
-myAny even [1 ..] == True
--}
 -- Notice we don't hit bottom - even though we `take 4` on a list with only 2 items
-fr1 = length $ take 2 $ take 4 xs
-  where
-    xs = [1, 2] ++ undefined
+fr1 = length $ take 2 $ take 4 xs where xs = [1, 2] ++ undefined -- 2
 
 -- No bottom here either
-fr2 = foldr (\_ _ -> 9001) 0 xs
-  where
-    xs = [undefined, undefined]
+fr2 = foldr (\_ _ -> 9001) 0 xs where xs = [undefined, undefined] -- 9001
+
+-- Or here
+fr3 = foldr (+) 0 xs where xs = take 4 [1, 2, 3, 4, undefined] -- 10
+
+-- Or here - `const` throws away the 2nd argument ("the rest of the fold")
+fr4 = foldr const 0 [1, undefined] -- 1
 
 --
--- Fold left
+-- 10.5 - Fold left
 --
+
 -- Compare:
 --
 {-
@@ -41,7 +45,7 @@ fr2 = foldr (\_ _ -> 9001) 0 xs
   1 ^ 512
   1
 -}
-e03 = foldr (^) 2 [1 .. 3]
+fl1 = foldr (^) 2 [1 .. 3]
 
 {-
   Left-associative:
@@ -50,52 +54,67 @@ e03 = foldr (^) 2 [1 .. 3]
   4 ^ 3
   64
 -}
-e04 = foldl (^) 2 [1 .. 3]
+fl2 = foldl (^) 2 [1 .. 3]
 
 -- Exercises: Understanding Folds
---
+
+-- 1
+
 uf1 = foldr (*) 1 [1 .. 5] -- 120
 
 -- uf1a = flip (*) 1 [1..5]         -- Syntax error
 uf1b = foldl (flip (*)) 1 [1 .. 5] -- 120
-
 uf1c = foldl (*) 1 [1 .. 5] -- 120
 
 uf2 = foldl (flip (*)) 1 [1 .. 3]
   -- (3 * (2 * 1))
 
+-- 2
+
+{-
+foldl (flip (*)) 1 [1..3]
+== ((1 * 1) * 2) * 3
+== (1 * 2) * 3
+== 2 * 3
+== 6
+-}
+
 -- 3: c
+
 -- 4: a
---
-uf5a = foldr (++) "" ["woot", "WOOT", "woot"]
 
-uf5b = foldr max ' ' "fear is the little death"
+-- 5
 
-uf5c = foldr (&&) True [False, True]
+uf5a = foldr (++) "" ["woot", "WOOT", "woot"] -- "wootWOOTwoot"
 
-uf5d = foldr (||) False [False, True]
+uf5b = foldr max ' ' "fear is the little death" -- 't'
 
-uf5e = foldr ((++) . show) "" [1 .. 5]
+uf5c = foldr (&&) True [False, True] -- False
 
-uf5f = foldr const 'a' ['1' .. '5']
+uf5d = foldr (||) False [False, True] -- True
 
-uf5g = foldr const '0' "tacos"
+uf5e = foldr ((++) . show) "" [1 .. 5] -- "12345"
 
-uf5h = foldl (flip const) '0' "burritos"
+uf5f = foldr const 'a' ['1' .. '5'] -- '1'
 
-uf5i = foldl (flip const) 'z' ['1' .. '5']
+uf5g = foldr const '0' "tacos" -- 't'
+
+uf5h = foldl (flip const) '0' "burritos" -- 's'
+
+uf5i = foldl (flip const) 'z' ['1' .. '5'] -- '5'
 
 --
 -- 10.6 - How to write fold functions
 --
-hwff1 = foldr (\a b -> take 3 a ++ b) "" ["Pizza", "Apple", "Banana"]
+
+hwff1 = foldr (\a b -> take 3 a ++ b) "" ["Pizza", "Apple", "Banana"] -- "PizAppBan"
 
 -- Exercises: Database Processing
---
+
 data DatabaseItem
   = DbString String
   | DbNumber Integer
-  | DbDate UTCTime
+  | DbDate   UTCTime
   deriving (Eq, Ord, Show)
 
 theDatabase :: [DatabaseItem]
@@ -106,55 +125,73 @@ theDatabase =
   , DbDate (UTCTime (fromGregorian 1921 5 1) (secondsToDiffTime 34123))
   ]
 
--- Again with the cheating. I'm guessing the author wants us to use folds here
+-- Again with the cheating. I thought the author wants us to use folds here
 -- but I don't see how foldr or foldl applies for these.
+
 {-
-filterDbDate theDatabase == [1911-05-01 09:28:43 UTC,1921-05-01 09:28:43 UTC
+filterDbDate theDatabase -- [1911-05-01 09:28:43 UTC,1921-05-01 09:28:43 UTC
 -}
 filterDbDate :: [DatabaseItem] -> [UTCTime]
-filterDbDate db = [date | (DbDate date) <- db]
+filterDbDate db = [ date | (DbDate date) <- db ]
 
 {-
-filterDbNumber theDatabase == [9001]
+filterDbNumber theDatabase -- [9001]
 -}
 filterDbNumber :: [DatabaseItem] -> [Integer]
-filterDbNumber db = [number | (DbNumber number) <- db]
+filterDbNumber db = [ number | (DbNumber number) <- db ]
 
 {-
-mostRecent theDatabase == 1921-05-01 09:28:43 UTC
+mostRecent theDatabase -- 1921-05-01 09:28:43 UTC
 -}
 mostRecent :: [DatabaseItem] -> UTCTime
 mostRecent = maximum . filterDbDate
 
 {-
-sumDb theDatabase == 9001
+sumDb theDatabase -- 9001
 -}
 sumDb :: [DatabaseItem] -> Integer
 sumDb = sum . filterDbNumber
 
 {-
-avgDb theDatabase == 9001.0
+avgDb theDatabase -- 9001.0
 -}
 avgDb :: [DatabaseItem] -> Double
 avgDb db = fromIntegral (sum aFilter) / fromIntegral (length aFilter)
-  where
-    aFilter = filterDbNumber db
+  where aFilter = filterDbNumber db
 
 --
 -- 10.9 - Scans
 --
+
+-- WARN: Infinite list!
+{-
+take 10 fibs -- [1,1,2,3,5,8,13,21,34,55]
+-}
 fibs :: [Integer]
 fibs = 1 : scanl (+) 1 fibs
 
+{-
+fib 10 -- 89
+-}
 fib :: Int -> Integer
-fib = (!!) fibs
+fib = (fibs !!)
 
 -- Scans Exercises
---
+
+{-
+fibs' -- [1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765]
+-}
 fibs' :: [Integer]
-fibs' = take 20 $ 1 : scanl (+) 1 fibs
+fibs' = take 20 $ 1 : scanl (+) 1 fibs'
 
+{-
+fibs'' -- [1,1,2,3,5,8,13,21,34,55,89]
+-}
 fibs'' :: [Integer]
-fibs'' = filter (< 100) (1 : scanl (+) 1 fibs)
+fibs'' = takeWhile (< 100) (1 : scanl (+) 1 fibs'')
 
-factorials = take 5 $ scanl (*) 1 [2 ..]
+{-
+factorials 5 -- [1,2,6,24,120]
+-}
+factorials :: Int -> [Int]
+factorials n = take n $ scanl (*) 1 [2 ..]
