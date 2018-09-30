9d48c999e0607b1252c41105e2ae989d4cac4f6e
Patrick Huffer <rhywun@rhywun.com>
2018-09-30 02:53:55 -0400

    Utterly lost in Chapter25

--- Chapter25/Scratch.hs
+++ Chapter25/Scratch.hs
@@ -9,22 +9,32 @@ module Chapter25.Scratch where
 -- "functors and applicatives are both closed under composition"
 --    (composing two of them returns another one of them)
 -- "this is not true of monads"
+-- "a monad transformer is a type constructor that takes a monad as an argument"
+-- "monad transformers are never sum or product types"
 
 --
 -- 25.2 - Common functions as types
 --
 
--- "a monad transformer is a type constructor that takes a monad as an argument"
-
-newtype Identity a = Identity { runIdentity :: a }
-
--- "monad transformers are never sum or product types"
+{-
+Identity :: * -> *
+      id :: a -> a
+-}
+newtype Identity a =
+  Identity { runIdentity :: a }
+  deriving (Eq, Show)
 
 {-
+Compose :: (* -> *) -> (* -> *) -> * -> *
+    (.) :: (b -> c) -> (a -> b) -> a -> c
+
 Compose [Just 1,Nothing] -- Compose {getCompose = [Just 1,Nothing]}
 
-Compose [Just (1::Int),Nothing] :: Compose [] Maybe Int
+Compose [Just (1::Int),Nothing] :: Compose []   Maybe Int
+                                           -f-  --g-- -a-
 -}
+-- remember, `f` and `g` are type constructors, not term-level functions, and `a` is a value type
+--              v v v
 newtype Compose f g a =
   Compose { getCompose :: f (g a) }
   deriving (Eq, Show)
@@ -33,38 +43,128 @@ newtype Compose f g a =
 -- 25.3 - Two little functors sittin' in a tree, L-I-F-T-I-N-G
 --
 
+{-
+fmap (+1) (Identity 2) -- Identity {runIdentity = 3}
+-}
 instance Functor Identity where
   fmap f (Identity a) = Identity (f a)
 
+{-
+fmap (+1) (Compose [Just 1,Nothing]) -- Compose {getCompose = [Just 2,Nothing]}
+-}
 instance (Functor f, Functor g) => Functor (Compose f g) where
   fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
 
---
+-- one less bit of structure than Compose
 
 newtype One f a = One (f a) deriving (Eq, Show)
 
 instance Functor f => Functor (One f) where
   fmap f (One fa) = One $ fmap f fa
 
---
+-- one more layer of structure than Compose
 
 newtype Three f g h a = Three (f (g (h a))) deriving (Eq, Show)
 
 instance (Functor f, Functor g, Functor h) => Functor (Three f g h) where
   fmap f (Three fgha) = Three $ (fmap . fmap . fmap) f fgha
 
---
+-- we can express arbitrarily-nested types
 
 v :: Compose [] Maybe (Compose Maybe [] Integer)
 v = Compose [Just (Compose $ Just [1])]
 
 --
 -- 25.4 - Twinplicative
---
+-- Cheated
 
 instance (Applicative f, Applicative g) => Applicative (Compose f g) where
   pure :: a -> Compose f g a
-  pure = undefined
+  pure fga = Compose (pure . pure $ fga)
 
   (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
-  (Compose f) <*> (Compose a) = undefined
+  (Compose f) <*> (Compose fga) = Compose $ (<*>) <$> f <*> fga
+
+--
+-- 25.5 - Twonad?
+--
+
+instance (Monad f, Monad g) => Monad (Compose f g) where
+  return = pure
+
+  (>>=) :: Compose f g a -> (a -> Compose f g b) -> Compose f g b
+  (>>=) = undefined                                   -- Impossible (see text)
+
+--
+-- 25.7 - Monad transformers
+--
+
+-- "multiple things going on"
+-- "like IO for effectful actions plus Reader for database connections"
+
+-- Doing it badly: one-off types
+
+newtype MaybeIO a = MaybeIO { runMaybeIO :: IO (Maybe a) }
+
+newtype MaybeList a = MaybeList { runMaybeList :: [Maybe a] }
+
+-- but we can do better
+-- "as long as we know what one of the types is"
+
+--
+-- 25.8 - IdentityT
+--
+
+-- Recall from above - additional structure *could* exist:
+{-
+newtype Identity a =
+  Identity { runIdentity :: a }
+  deriving (Eq, Show)
+-}
+
+-- A transformer - additional structure *does* exist
+newtype IdentityT f a =
+  IdentityT { runIdentityT :: f a }
+  deriving (Eq, Show)
+
+-- Again, review:
+{-
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+-}
+
+instance (Functor m) => Functor (IdentityT m) where
+  fmap f (IdentityT fa) = IdentityT (fmap f fa)
+
+-- Review:
+{-
+instance Applicative Identity where
+  pure = Identity
+
+  (Identity f) <*> (Identity a) = Identity (f a)
+-}
+
+instance (Applicative m) => Applicative (IdentityT m) where
+  pure x = IdentityT (pure x)
+
+  (IdentityT fab) <*> (IdentityT fa) = IdentityT (fab <*> fa)
+--           ^^^
+--        m (a -> b)
+
+-- Yada yada
+{-
+instance Monad Identity where
+  return = pure
+
+  (Identity a) >>= f = f a
+-}
+
+instance (Monad m) => Monad (IdentityT m) where
+  return = pure
+
+  (>>=) :: IdentityT m a -> (a -> IdentityT m b) -> IdentityT m b
+  -- (IdentityT ma) >>= f = IdentityT $ ma >>= runIdentityT . f
+  (IdentityT ma) >>= f = undefined
+
+--
+-- totally lost
